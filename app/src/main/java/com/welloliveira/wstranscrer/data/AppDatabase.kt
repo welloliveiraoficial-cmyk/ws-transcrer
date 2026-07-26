96e38d38c4e799d545fd7e038ec52c02d5621262
welloliveiraoficial-cmyk <welloliveiraoficial@gmail.com>
2026-07-25 22:41:50 -0300

    Create AppDatabase.kt

---
+++ app/src/main/java/com/welloliveira/wstranscrer/data/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.welloliveira.wstranscrer.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Transcricao::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun transcricaoDao(): TranscricaoDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun get(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "ws_transcrer.db"
+                ).build().also { INSTANCE = it }
+            }
+    }
+}
