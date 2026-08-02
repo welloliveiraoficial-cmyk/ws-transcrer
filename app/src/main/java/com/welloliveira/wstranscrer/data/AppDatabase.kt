bed068f4a746aaafac5528b7eb8adef71e4afa39
welloliveiraoficial-cmyk <welloliveiraoficial@gmail.com>
2026-08-02 05:55:38 -0300

    Update AppDatabase.kt

--- app/src/main/java/com/welloliveira/wstranscrer/data/AppDatabase.kt
+++ app/src/main/java/com/welloliveira/wstranscrer/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Transcricao::class], version = 1, exportSchema = false)
+@Database(entities = [Transcricao::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun transcricaoDao(): TranscricaoDao
 
@@ -18,7 +18,7 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "ws_transcrer.db"
-                ).build().also { INSTANCE = it }
+                ).fallbackToDestructiveMigration().build().also { INSTANCE = it }
             }
     }
 }
